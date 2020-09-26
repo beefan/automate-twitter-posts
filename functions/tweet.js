a0c14462029cefb0dba50c5f11524fc51bf044fd
Brandon Fannin <bfannin13@gmail.com>
2020-09-26 01:56:12 -0400

    fixup for netlify setup

--- functions/tweet.js
+++ functions/tweet.js
@@ -1,19 +1,20 @@
 // uncomment to run with local .env file
-// require('dotenv').config()
+//require('dotenv').config()
 
 const OAuth = require('oauth');
 
 // uncomment to test tweet function and api keys locally
-// tweet('Was looking for a way to process markdown files in vue and found this. Pretty cool! https://vue-showdown.js.org/guide/#npm')
+//tweeter('cleaning up some tests')
 
 // export function to netlify endpoint
 exports.handler = async event => {
     const tweet = event.queryStringParameters.t
-    tweeter(tweet);
+    console.log(tweet);
+    return tweeter(tweet);
 }
 
 function tweeter(text) {
-    const authobj = new OAuth.OAuth(
+    const twitter = new OAuth.OAuth(
         'https://api.twitter.com/oauth/request_token',
         'https://api.twitter.com/oauth/access_token',
         process.env.TWITTER_API_KEY,
@@ -23,28 +24,24 @@ function tweeter(text) {
         'HMAC-SHA1'
     );
 
-    var postBody = {
-        'status': text
-    };
-
-    authobj.post('https://api.twitter.com/1.1/statuses/update.json',
+    twitter.post('https://api.twitter.com/1.1/statuses/update.json',
         process.env.TWITTER_ACCESS_TOKEN,
         process.env.TWITTER_TOKEN_SECRET,
-        postBody,
+        { "status": text },
         'application/json',
         function (err, data, res) {
             if (err) {
                 console.error(err)
-                return {
+                callback(null, {
                     statusCode: 500,
                     body: "Failed to send tweet"
-                }
+                })
             } else {
                 console.log('good')
-                return {
+                callback(null, {
                     statusCode: 200,
                     body: "Sent tweet successfully"
-                }
+                });
             }
         });
 }
